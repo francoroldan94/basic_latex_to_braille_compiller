b64c9b4bcc2ff18d172f2493063378ce2baf437a
franco <franco@franco-System-Product-Name>
2018-04-09 01:01:46 -0300

    Le agregué un saludo al mundo

--- MateBraille/src/matebraille/MateBraille.java
+++ MateBraille/src/matebraille/MateBraille.java
@@ -16,6 +16,7 @@ public class MateBraille {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Hola mundo");
     }
     
 }
